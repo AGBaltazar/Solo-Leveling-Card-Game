bad9752a9c36bbb0670a9df98082e5c64d82b077
Alejandro Garcia Baltazar <agee.baltazar@gmail.com>
2022-07-26 06:43:35 -0700

    Added game engine and player objects

---
+++ Mechanics/Gameplay.js
@@ -0,0 +1,17 @@
+
+
+let gameEngine()={
+    gameStart: function() {
+        initPlayer;
+        loadEnemies;
+    },
+    initPlayer: function(){
+        player = new player(1, 100, 10, 10, 10);
+
+        let loadStats = document.querySelector('.attributes'.innerHTML = 
+        '<p>Level'+player.level+'</p>')
+     },
+     loadEnemies: function(){
+
+     }
+}
---
+++ Mechanics/Player.js
@@ -0,0 +1,9 @@
+let player;
+
+function player(level, health, attack, speed, magic){
+    this.level = level;
+    this.health = health;
+    this.attack = attack;
+    this.speed = speed;
+    this.magic = magic;
+}
